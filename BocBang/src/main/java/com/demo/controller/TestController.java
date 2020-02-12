611780448cb403ad90a692103294fb3ff29d3abf
tienph <tienph@rikkeisoft.com>
2020-02-12 14:11:51 +0700

    change 400->500

--- BocBang/src/main/java/com/demo/controller/TestController.java
+++ BocBang/src/main/java/com/demo/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.demo.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;400
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/all")
     public ResponseEntity<Object> findLocationById() {
-        ResponseEntity<Object> object = new ResponseEntity<Object>(400, HttpStatus.OK);
+        ResponseEntity<Object> object = new ResponseEntity<Object>(500, HttpStatus.OK);
         return object;
     }
 }
