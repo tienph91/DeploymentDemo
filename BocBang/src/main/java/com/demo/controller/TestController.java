a290096bba2a99461db1cdb73326906686ddf71d
tienph <tienph@rikkeisoft.com>
2020-02-13 14:24:54 +0700

    test

--- BocBang/src/main/java/com/demo/controller/TestController.java
+++ BocBang/src/main/java/com/demo/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.demo.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;400
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
